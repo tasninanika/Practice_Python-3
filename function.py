3a40124893fe870f001d988fd847fd2bc5e46689
anikatasnin <jarintasnin09@gmail.com>
2025-01-07 21:27:21 +0600

    updated function

--- function.py
+++ function.py
@@ -5,7 +5,16 @@ Created on Tue Jan  7 21:08:40 2025
 @author: Jarin
 """
 
+# static function
 def gretting():
     print('Jarin Tasnin Anika')
 
 gretting()
+
+
+# dynamic function
+
+def my_print(x):
+    print(x)
+
+my_print('Jarin')
